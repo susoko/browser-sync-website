323afa637b259a99774ded4c766d36c038e0a32d
Shane Osbourne <shane.osbourne8@gmail.com>
2014-10-29 17:23:53 +0000

    Document for recursive file watching - fix https://github.com/shakyShane/browser-sync/issues/294

--- _includes/snippets/options/files.js
+++ _includes/snippets/options/files.js
@@ -4,8 +4,8 @@ files: "app/css/style.css"
 // multiple files
 files: ["app/css/style.css", "app/css/ie.css"]
 
-// multiple files with glob
-files: "app/css/*.css"
+// watch all files under app/css (including sub directories)
+files: "app/css/**"
 
 // multiple globs
 files: ["app/css/*.css", "app/**.*.html", "app/js/**/*.js"]
